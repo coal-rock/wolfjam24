78ffdca92f84f180ab63fcf1f3917d3e00cd7388
Deborah Conti <debair@dyn-169-226-242-82.wireless.albany.edu>
2024-11-16 14:53:24 -0500

    irl qte countdown

--- IRLQTE.gd
+++ IRLQTE.gd
@@ -5,10 +5,17 @@ var irlgames = ["Thumb War!!", "Rock Paper Scissors!!"]
 var allowinp = false
 
 func _ready() -> void:
-	$Label.text = irlgames[randi() % len(irlgames)]
-	await get_tree().create_timer(3).timeout
+	var game = irlgames[randi() % len(irlgames)]
+	$Label.text = game
+	await get_tree().create_timer(1).timeout
+	$Label.text = game + "\n in 3.."
+	await get_tree().create_timer(1).timeout
+	$Label.text = game + "\n in 3.. 2.."
+	await get_tree().create_timer(1).timeout
+	$Label.text = game + "\n in 3.. 2.. 1.."
+	await get_tree().create_timer(1).timeout
 	allowinp = true
-	$Label.text = "who won? (press key)"
+	$Label.text = game + "\n\n who won? (press key)"
 
 func _process(delta: float) -> void:
 	if allowinp:
