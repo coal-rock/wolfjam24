0a74c16ab64f7d50212ef8e41289945037ca6fa6
coal-rock <colebrazinski15@gmail.com>
2024-11-16 09:47:08 -0500

    refactor dice logic

--- scenes/Battle.gd
+++ scenes/Battle.gd
@@ -6,31 +6,24 @@ extends Node2D
 @export var inputName1: String
 @export var inputName2: String
 
-
-func roll(sides: int) -> int:
-	if sides <= 0:
-		return 0  # Invalid dice
-	return randi() % sides + 1
-
-func update_die(r: DiceRoller, sides: int) -> void:
-	var roll: int = roll(6)
-	r.spr.frame = roll - 1
-	r.is_rolled = true
+func handle_roll(r: DiceRoller) -> void:
+	r.update_die()
 	
-	# now if both have a roll, start the battle
 	if dice1.is_rolled && dice2.is_rolled:
 		print("BOTH ROLL")
 		dice1.is_rolled = false
 		dice2.is_rolled = false
+		
+		# now if both have a roll, start the battle	
 		if dice1.spr.frame == dice2.spr.frame:
 			print("BOTH MATCH")
 
 func _on_ready() -> void:	
-	update_die(dice1, 6)
-	update_die(dice2, 6)
+	handle_roll(dice1)
+	handle_roll(dice2)
 
 func _process(delta):
 	if Input.is_action_just_pressed(inputName1):
-		update_die(dice1, 6)
+		handle_roll(dice1)
 	if Input.is_action_just_pressed(inputName2):
-		update_die(dice2, 6)
+		handle_roll(dice2)
--- scripts/DiceRoller.gd
+++ scripts/DiceRoller.gd
@@ -3,9 +3,16 @@ extends Node
 class_name DiceRoller
 @export var is_rolled = false;
 @export var spr: Sprite2D;
+@export var sides: int = 6;
 
 func _ready() -> void:
 	spr = $Sprite2D
 	print(spr)
 	
-	
+func roll() -> int:
+	return randi() % sides + 1
+
+func update_die() -> void:
+	var roll: int = roll()
+	spr.frame = roll - 1
+	is_rolled = true
