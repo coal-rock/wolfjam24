4d020ea0a4cca150b7938a90e2610ec5fee99988
Deborah Conti <debair@dyn-169-226-81-128.wireless.albany.edu>
2024-11-15 22:26:46 -0500

    qte tests

---
+++ qte.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+var inputs = [KEY_W, KEY_A, KEY_S, KEY_D]
+var selectedinput = -1
+var timeleft = -1
+
+@onready
+var mylabel = get_node("Node2D/Label")
+
+func _ready() -> void:
+	reinit()
+	
+func reinit() -> void:
+	timeleft = 300
+	selectedinput = inputs[randi() % len(inputs)]
+
+var byp = 0
+
+func _process(delta: float) -> void:
+	if byp == 1:
+		return
+	timeleft -= 1
+	mylabel.text = "PRESS %s (%s)" % [ OS.get_keycode_string(selectedinput),timeleft]
+	if Input.is_key_pressed(selectedinput):
+		mylabel.text = "pressed!"
+		byp = 1
+		await get_tree().create_timer(1).timeout
+		byp = 0
+		reinit()
+	if timeleft <= 0:
+		mylabel.text = "time ran out"
+		
+	
