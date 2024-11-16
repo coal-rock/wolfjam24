a16552ea9dac493f266df6eb08d127debb4ffca4
Andrew L <129340220+AndrewLukan@users.noreply.github.com>
2024-11-16 15:29:13 -0500

    hm

--- scripts/opening.gd
+++ scripts/opening.gd
@@ -1,18 +1,10 @@
-extends RichTextLabel
+extends Control
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-var opening_text = "This is some example text.\nThis is a newline."
-
-func _ready() -> void:
-	scroll_text(opening_text)
-	
-func scroll_text(input_text:String) -> void:
-	visible_characters = 0
-	text = input_text
-	
-	for i in get_parsed_text():
-		visible_characters += 1
-		await get_tree().create_timer(0.1).timeout
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Play the fade-in sequence animation
+	$AnimationPlayer.play("FadeIn")
