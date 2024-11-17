99c2d81a8f94d6c790143130061763d6d1b129d7
Andrew L <129340220+AndrewLukan@users.noreply.github.com>
2024-11-16 19:40:07 -0500

    Exit Button Works

--- scripts/opening.gd
+++ scripts/opening.gd
@@ -1,18 +1,27 @@
 extends Control
 
-signal fade_in_complete
+signal text_fade_in_complete
 
 @export var next_scene: String = "res://scenes/battle.tscn"
 
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	# Play the fade-in sequence animation
-	$AnimationPlayer.play("FadeIn")
-	print("Fade in called. Starting...")
+	# Connect the signal once in _ready()
+	if not $AnimationPlayer.is_connected("animation_finished", Callable(self, "_on_fade_out_complete")):
+		$AnimationPlayer.connect("animation_finished", Callable(self, "_on_fade_out_complete"))
+		$AnimationPlayer.play("FadeIn")
+		print("Fade-in called. Starting...")
 
 func on_fade_in_complete():
-	emit_signal("fade_in_complete")
-	print("Fade-in completed. Changing scene.")
-	get_tree().change_scene_to_file("res://scenes/battle.tscn")
+	emit_signal("text_fade_in_complete")
+	print("Text fade-in completed. Preparing scene transition...")
+	transition_to_next_scene()
+
+# Start fade-out and wait for it to complete before transitioning scenes
+func transition_to_next_scene():
+	print("Starting fade-out...")
+	$AnimationPlayer.play("FadeOut")
+
+func _on_fade_out_complete(anim_name: String):
+	if anim_name == "FadeOut":
+		print("Fade-out completed. Changing scene...")
+		get_tree().change_scene_to_file("res://scenes/battle.tscn")
