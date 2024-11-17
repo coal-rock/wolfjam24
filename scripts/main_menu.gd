d71378bc6babd92ffa0b6ea3af0b7a6773ab1be3
Andrew L <129340220+AndrewLukan@users.noreply.github.com>
2024-11-16 21:30:18 -0500

    Transitions Between Scenes Function

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,7 +6,8 @@ func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/opening.tscn")
 
 func _on_credits_button_pressed():
+	print("Credits button pressed")
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
-func _on_quit_button_pressed() -> void:
+func _on_quit_button_pressed():
 	get_tree().quit()
