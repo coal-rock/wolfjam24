6097d44435e9bbe89cf6564afac25424176dd9ac
Andrew L <129340220+AndrewLukan@users.noreply.github.com>
2024-11-16 21:32:09 -0500

    Revert "Transitions Between Scenes Function"
    This reverts commit d71378bc6babd92ffa0b6ea3af0b7a6773ab1be3.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,8 +6,7 @@ func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/opening.tscn")
 
 func _on_credits_button_pressed():
-	print("Credits button pressed")
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
-func _on_quit_button_pressed():
+func _on_quit_button_pressed() -> void:
 	get_tree().quit()
